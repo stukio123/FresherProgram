180ee0f137876c4289684b1542a20ce958bc8640
Phạm Trí Quang <stukio123@gmail.com>
2021-03-06 10:33:21 +0700

    Initial First Commit

---
+++ src/main/java/com/fresherprogram/controller/renewController.java
@@ -0,0 +1,10 @@
+package com.fresherprogram.controller;
+
+public class renewController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/fresherprogram/util/Configuration.java
@@ -0,0 +1,10 @@
+package com.fresherprogram.util;
+
+public class Configuration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
